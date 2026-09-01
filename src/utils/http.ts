8babb0573ddefa402d6d7fbe6ce5ffc85248c3cf
Mindy <16881183+mindygitee@user.noreply.gitee.com>
2026-09-01 14:56:27 +0800

    feat:打包项目

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,7 +2,7 @@
 import {store} from "@/store";
 import axios,{AxiosInstance,InternalAxiosRequestConfig,AxiosResponse} from "axios";
 
-console.log(process.env.REACT_APP_URL)
+// console.log(process.env.REACT_APP_URL)
 
 const http:AxiosInstance = axios.create({
   baseURL:"",
